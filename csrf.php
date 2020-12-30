0a7649b8a224faedaf17e2789d024140494963bf
MRCKER-BOD0 <76541771+MRCKER-BOD0@users.noreply.github.com>
2020-12-30 23:47:36 +0700

    Create csrf.php

---
+++ csrf.php
@@ -0,0 +1,57 @@
+ 
+<!-- JANGAN LUPA SUBSCRIBE HACKDITNIME CHANNEL + LIKE + BANTU SHARE LURR :) -->
+<!doctype html>
+<head>
+<title>CSRF ONLINE F3RGUSO </title>
+<meta content="CSRF ONLINE" name="title">
+<meta content="CSRF ONLINE" name="description">
+<meta content="CSRF ONLINE" name="keywords">
+<meta content="CSRF ONLINE" name="Abstract">
+</head>
+<style type="text/css">
+body{background-image:url(https://i.gifer.com/7zon.gif); background-size:cover}
+</style>
+<body>
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/daun-berguguran.js" type="text/javascript"></script> 
+<center>
+<b><h3><font face="courier new" color="white" style="text-shadow: 0px 1px 5px red;"> CSRF ONLINE BY MRCKER</font></h3></b>
+<audio src="https://f.top4top.io/m_1825isujb0.mp3" controls="controls" autoplays="autoplays" type="audio/mpeg"></audio>
+<br>
+<img src="https://d.top4top.io/p_1825yjfbk0.png" weight="300" height="300">
+<br>
+<marquee behavior="alternate" direction="center">
+<b>
+<h4>
+<font face="courier new" color="white" style="text-shadow: 0px 1px 5px red;"> Thanks For All Subscriber,All Friends,All Member,All Cyber</font>
+</h4>
+</b>
+</marquee>
+<br>
+<br>
+   <div style="display">		
+                                               <center>
+<form method="post">
+<font face="Inconsolata" color="white"> URL Target : </font> <font color="red"> &#1203;&#824;&#1202;&#824;&#1203; </font> <input type="text" name="url" placeholder="Your Target" style="margin: 5px auto; padding-left: 5px;" required>
+<br><br><font face="Inconsolata" color="white"> Post File : </font> <font color="red">&#1203;&#824;&#1202;&#824;&#1203; </font><font color="black">-</font>  <input type="text" name="pf" size="50" height="10" placeholder="Filedata / files[] / qqfile / userfile / dll" required>
+<br><br><input type = "submit" name = "submit" value = "Target Key" class="login-input"style='width: 130px; height: 40px;'> </ form >
+</form>
+</form>
+<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript">
+  $(window).load(function() {
+   $("#content").fadeOut();
+   $("#preloader").delay(10).fadeOut("slow");
+  })
+    </script>
+    <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p03.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582NzYpoUazw5m1yEw0Zhss6dvuHT13xMZiBN37rSBhkrd%2fCI6B%2fO93ucRMvQCK4bk0WK%2bjVW9CZiim%2buqGrq0rZWmRqTaXCl13wBkXBaFmn5VyZYrEN%2brCEiUUv6PpHM%2fO5GRaJa2GVhiytVD8xjnt2oLcufZ12xvW1ulina2Rp%2fKFtFfU2SOAaIpMUNhsEmcAANAx2xbX0gOAEQ39FTx4l%2b0avPk9KlsWYDybRimp4YVR4x8vs8IvKwzsnHbA4dlWic8yZqScxxsiZE7U7iKfHuK%2fLHrPzq16ygpWj2dgoty%2fPbhMCbBJ%2bSGQdnm41dFpHbMOGKCs1X4kALbvo7%2ftcjaSsFbm0rhPdmmhoNlTYzqlUzBbkmagEbYz7suV%2buW3HhiNKdlobKcd7TUXRwV2tuumevdkz6ByjBcGh%2fc%2brHEE%2fvTuIcIk0NZbLPmDVwqbIWaWC7eeWDiuPN7cuMK%2fUHyQtdhDdTFbdyu7aa7XDtY7P3ptscirhAEN7wf3287hvdZJzqCj1clRB1X1N9EaytwJv3rBiqZJESyMLFie3VfNGJGpwGfypQCe%2bX4jNF5KYKZNxbbqFAK8rjFbeqRT%2fdTawUgcFBBww%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script>
+                               <br>    
+                      <right>
+                      <b>
+                      <h4>
+                      <font face="courier new" color="white" style="text-shadow: 0px 1px 5px red;">© MRCKER <br> Kalo Ada Yang Mudah,Kenapa Harus Yang Susah ?</font>
+                      </h4>
+                      </b>
+                      </right>
+                </center>
+                </body>
+                </html>
